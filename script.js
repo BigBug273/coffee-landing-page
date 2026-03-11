c27d72575c357a574785b6c03c0c6ef76013b948
BigBug273 <162987814+BigBug273@users.noreply.github.com>
2026-03-11 18:34:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function orderCoffee(){
 
-alert("Thank you for choosing BeanCraft Coffee ☕");
+alert("Thanks for choosing BeanCraft Coffee ☕");
 
 }
+
+
+/* FAQ accordion */
+
+const questions = document.querySelectorAll(".faq-question")
+
+questions.forEach(q=>{
+
+q.addEventListener("click",()=>{
+
+const answer = q.nextElementSibling
+
+answer.style.display =
+answer.style.display === "block"
+? "none"
+: "block"
+
+})
+
+})
